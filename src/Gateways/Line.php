da5a74be36d31c6a12dbe5fa54acb3966e54de50
Coeus <r.anerg@gmail.com>
2018-12-20 18:19:30 +0800

    client line

--- src/Gateways/Line.php
+++ src/Gateways/Line.php
@@ -83,7 +83,7 @@ class Line extends Gateway
             'uri'    => self::API_BASE . $api,
         ];
 
-        $headers = ['Authorization' => $this->token['token_type'] . ' ' . $this->token['access_token']];
+        $headers = ['Authorization' => isset($this->token['token_type']) ? $this->token['token_type'] : 'Bearer' . ' ' . $this->token['access_token']];
 
         $data = $this->$method($request['uri'], $params, $headers);
 
